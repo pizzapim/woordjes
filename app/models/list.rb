383bd9e22393fcef26e323330f50b8eae380ca53
Pim Kunis <pim@kunis.nl>
2018-07-24 17:30:17 +0200

    enable deleting links

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,7 +2,14 @@ class List < ApplicationRecord
   # Associations
   belongs_to :user
   has_many :links, index_errors: true, dependent: :destroy
-  accepts_nested_attributes_for :links, allow_destroy: true, reject_if: proc {|attributes| attributes['word1'].blank? && attributes['word2'].blank?}
+  accepts_nested_attributes_for :links, allow_destroy: true, reject_if: :reject_links
+
+  def reject_links(attributes)
+    exists = attributes['id'].present?
+    empty = attributes.slice(:word1, :word2).values.all?(&:blank?)
+    attributes.merge!({:_destroy => 1}) if exists && empty
+    return (!exists && empty) # reject empty attributes if they don't exist
+  end
 
   # Callbacks
   before_validation do
