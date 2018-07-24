e6644021157726a34c797202c64f07bd11682a66
Pim Kunis <pim@kunis.nl>
2018-07-24 17:00:20 +0200

    change the association fields helper only for links put the extra fields in a js var and not in a data field add jquery ready selector

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -1,11 +1,11 @@
 function add_fields(link, association, content) {
   var new_id = new Date().getTime();
   var regexp = new RegExp("new_" + association, "g");
-  $(link).before(content.replace(regexp, new_id).repeat(10));
+  $(link).before(content.replace(regexp, new_id));
 }
 $(document).on('turbolinks:load', function() {
   $('#add_fields').click(function() {
-    add_fields(this, $(this).attr("data-association"), $(this).attr("data-fields"))
+    add_fields(this, "links", more_fields.repeat(10));
     return false;
   });
 });
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
-  def link_to_add_fields(name, f, association)
-    new_object = f.object.class.reflect_on_association(association).klass.new
-    fields = f.fields_for(association, new_object, child_index: "new_#{association}") do |builder|
-      render(association.to_s.singularize + "_fields", f: builder)
+  def more_fields(f)
+    new_link = f.object.class.reflect_on_association(:links).klass.new
+    fields = f.fields_for(:links, new_link, child_index: "new_links") do |builder|
+      render("link_fields", f: builder)
     end
-    link_to(name, "#", id: "add_fields", data: {association: association, fields: fields})
+    fields
   end
 
   def quiz_types
