3504142f0dee75d45ced5d1a558660713fd271e2
Pim Kunis <pim@kunis.nl>
2018-07-25 20:02:29 +0200

    save errors in an array

--- app/assets/javascripts/quiz.js
+++ app/assets/javascripts/quiz.js
@@ -1,9 +1,3 @@
-/*
-
-Depending on the quiz type, callbacks differ. Different things need to happen when the user presses 'check' for example. That's why there needs to be an initial configuration which callbacks to use:
-
-*/
-
 var quiz_conf = {};
 
 var list = {};
@@ -11,6 +5,7 @@ var list = {};
 var quizQuestions = [];
 var currentQuestion = 0;
 var state = "awaitingInput";
+var errors = [];
 
 $('body.quiz-quiz').ready(function () {
   quiz_conf.quiz_type = parsed.quiz.quiz_type;
@@ -27,6 +22,7 @@ $('body.quiz-quiz').ready(function () {
 function normalQuizInit() {
   // In a normal quiz, we randomize the order of the words. After that, we display the next question.
   quizQuestions = Object.assign([], parsed.list.links); // Warning! The individual links are a reference to parsed.list.links and should not be edited.
+  // TBD: don't duplicate the questions, but save the array indices and shuffle those.
   shuffleArray(quizQuestions);
   $("#check-answer").click(normalQuizCheck);
   $(document).keypress(function(e) {
@@ -48,6 +44,7 @@ function normalQuizCheck() {
     } else {
       $("#answer-feedback").html("Incorrect. The correct answer is: " + quizQuestions[currentQuestion][word(2)]);
       quizQuestions.splice(currentQuestion + 2 + Math.floor(Math.random() * 3), 0, quizQuestions[currentQuestion]);
+      errors.push({id: quizQuestions[currentQuestion].id, actualAnswer: quizQuestions[currentQuestion][word(2)], userAnswer: answer});
     }
     $("#answer-input").val("");
     currentQuestion++;
