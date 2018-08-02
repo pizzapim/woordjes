5a2885af1ac9d47d53b25959b4e13bb40df1d997
Pim Kunis <pim@kunis.nl>
2018-08-02 21:14:02 +0200

    add signup form on home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,20 @@ module ApplicationHelper
   def quiz_types
     ["normal"]
   end
+
+  def resource
+    @new_user || User.new
+  end
+
+  def resource_name
+    :user
+  end
+
+  def resource_class
+    User
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
