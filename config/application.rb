1ec6b50763b8d3677cb462fe310c3763339abf7c
Pim Kunis <pim@kunis.nl>
2018-08-22 22:28:47 +0200

    configure for production

--- config/application.rb
+++ config/application.rb
@@ -12,9 +12,11 @@ module Woordjes
     config.load_defaults 5.2
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value.to_s
+      end if File.exists?(env_file)
+    end
   end
 end
